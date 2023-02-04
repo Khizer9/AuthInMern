def17a46bc9a4259cdc59cf95fab0a233ad304b0
Khizer Ali <khizer358@gmail.com>
2023-02-04 17:06:45 +0500

    misc changes

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,4 +1,5 @@
 import styles from "./styles.module.css";
+import { BiMessageSquareAdd } from 'react-icons/bi';
 
 const Main = () => {
 	const handleLogout = () => {
@@ -9,12 +10,16 @@ const Main = () => {
 	return (
 		<div className={styles.main_container}>
 			<nav className={styles.navbar}>
-				<h1>fakebook</h1>
+				<h1>Logo Here</h1>
 				<button className={styles.white_btn} onClick={handleLogout}>
 					Logout
 				</button>
 			</nav>
+
+			<h1>Hello World</h1>
+			<BiMessageSquareAdd/>
 		</div>
+		
 	);
 };
 
